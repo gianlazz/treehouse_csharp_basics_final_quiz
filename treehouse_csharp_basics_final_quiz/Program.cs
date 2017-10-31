160e0ef20c34884d0e0cc58c0daf8b1c445b1508
Gian Lazzarini <gianlazzarini@gmail.com>
2017-10-31 14:45:38 -0700

    Initial Commit

---
+++ treehouse_csharp_basics_final_quiz/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Treehouse.CodeChallenges
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool keepGoing = true;
+
+            while (keepGoing)
+            {
+                Console.Write("Enter the number of times to print \"Yay!\": ");
+                string userEntry = Console.ReadLine();
+                int x = 0;
+
+                if(userEntry == "quit") {
+                    keepGoing = false;
+                    break;
+                }
+
+                try
+                {
+                    while (x < int.Parse(userEntry))
+                    {
+                        Console.Write("Yay!");
+                        x++;
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("You must enter a whole number.");
+                    continue;
+                }
+            }
+            Console.WriteLine("Code Execution Completed");
+        }
+    }
+}
